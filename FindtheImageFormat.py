ca931fa5fab86abb4effc24aafa651498f58f92b
Mert Efe Sevim <mertefesevim@gmail.com>
2017-06-17 11:57:29 +0300

    easier to read and work now

--- FindtheImageFormat.py
+++ FindtheImageFormat.py
@@ -1,25 +1,28 @@
-def photoExtension(fileName):
+def photoExtension():
+
+    fileName = input("File Name ? ")                            #It's better to ask here, why adding new func for this ?!
+
     try:
-        fh = open(fileName, 'rb').read(10)  #read the first 10 bytes of the image file
-        if fh[6:11] in [b'JFIF', b'Exif']: #If there are JFIF or Exif bytes between 7 and 10 bytes, jpeg
+        fh = open(fileName, 'rb').read(10)                      #read the first 10 bytes of the image file
+
+        if fh[6:11] in [b'JFIF', b'Exif']:                      #If there are JFIF or Exif bytes between 7 and 10 bytes, jpeg
             print("The extension of {} is JPEG".format(fileName))
-        elif fh[:8] == b'\211PNG\r\n\032\n':   #The first 8 bytes of png contain the following value: b'\x89PNG\r\n\x1a\n'
+
+        elif fh[:8] == b'\211PNG\r\n\032\n':                    #The first 8 bytes of png contain the following value: b'\x89PNG\r\n\x1a\n'
             print("The extension of {} is PNG".format(fileName))
-        elif fh[:3] == b'GIF':   #the first 3 bytes of gif contain: GIF
+
+        elif fh[:3] == b'GIF':                                  #the first 3 bytes of gif contain: GIF
             print("The extension of {} is GIF".format(fileName))
-        elif fh[:2] in [b'II', b'MM']:  #the first 2 bytes of TIFF contain : b'II' or b'MM'
+
+        elif fh[:2] in [b'II', b'MM']:                          #the first 2 bytes of TIFF contain : b'II' or b'MM'
             print("The extension of {} is TIFF".format(fileName))
-        elif fh[:2] in [b'BM']: #the first 2 bytes of BMP contain : b'BM'
+
+        elif fh[:2] in [b'BM']:                                 #the first 2 bytes of BMP contain : b'BM'
             print("The extension of {} is BMP".format(fileName))
 
     except FileNotFoundError:
         print("The file was not found...")
 
 
-def body():
-    fileName = input("File Name ? ")
-    photoExtension(fileName)
-
-
 if __name__ == '__main__':
-    body()
+    photoExtension()
